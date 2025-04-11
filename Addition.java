339626afc8a76050cf992db1e827861720e4fddd
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-04-11 21:58:29 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition {
+    double a, b;
+
+    public Addition(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double add() {
+        return a + b;
+    }
+}
+
