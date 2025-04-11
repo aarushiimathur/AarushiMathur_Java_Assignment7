8d41cbcdbff35fc66e671d8c1a3cd9432896e2f0
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-04-11 22:00:45 +0530

    Create SquareRoot.java

---
+++ SquareRoot.java
@@ -0,0 +1,11 @@
+public class SquareRoot {
+    private double a;
+
+    public SquareRoot(double a) {
+        this.a = a;
+    }
+
+    public double squareRoot() {
+        return Math.sqrt(a);
+    }
+}
