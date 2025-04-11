4104db8f77d130d211963a4d8b0eace90efb12b0
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-04-11 21:59:32 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication {
+    private double a, b;
+
+    public Multiplication(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double multiply() {
+        return a * b;
+    }
+}
